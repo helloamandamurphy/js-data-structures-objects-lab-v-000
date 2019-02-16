dacd0267fc784ce3ecb099d55c32335d81f21dab
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-16 18:15:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your solution in this file!
 const driver = {};
 
-function updateDriverWithKeyandValue(driver,key,value)
+function updateDriverWithKeyandValue(driver, key, value) {}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {}
+function deleteFromDriverByKey(driver, key) {}
+function destructivelyDeleteFromDriverByKey(driver, key) {}
